f549d5d18d41a492ecd0817ac7d21f58e86fa768
ikadgzl <ikadgzl@gmail.com>
2022-01-31 10:08:55 +0300

    added functionality with dynamic section design

--- nike-clone-ecommerce/app.js
+++ nike-clone-ecommerce/app.js
@@ -1,16 +1,124 @@
 const sliderWrapper = document.querySelector('.sliderWrapper');
 const menuItems = document.querySelectorAll('.menuItem');
 
-const handleSlider = () => {
-  menuItems.forEach((item, idx) => {
-    item.addEventListener('click', () => {
-      sliderWrapper.style.transform = `translateX(${-100 * idx}vw)`;
+const products = [
+  {
+    id: 1,
+    title: 'Air Force',
+    price: 119,
+    colors: [
+      {
+        code: 'black',
+        img: './img/air.png'
+      },
+      {
+        code: 'darkblue',
+        img: './img/air2.png'
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: 'Air Jordan',
+    price: 149,
+    colors: [
+      {
+        code: 'lightgray',
+        img: './img/jordan.png'
+      },
+      {
+        code: 'green',
+        img: './img/jordan2.png'
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: 'Blazer',
+    price: 109,
+    colors: [
+      {
+        code: 'lightgray',
+        img: './img/blazer.png'
+      },
+      {
+        code: 'green',
+        img: './img/blazer2.png'
+      }
+    ]
+  },
+  {
+    id: 4,
+    title: 'Crater',
+    price: 129,
+    colors: [
+      {
+        code: 'black',
+        img: './img/crater.png'
+      },
+      {
+        code: 'lightgray',
+        img: './img/crater2.png'
+      }
+    ]
+  },
+  {
+    id: 5,
+    title: 'Hippie',
+    price: 99,
+    colors: [
+      {
+        code: 'gray',
+        img: './img/hippie.png'
+      },
+      {
+        code: 'black',
+        img: './img/hippie2.png'
+      }
+    ]
+  }
+];
+
+const currentProductImg = document.querySelector('.productImg');
+const currentProductTitle = document.querySelector('.productTitle');
+const currentProductPrice = document.querySelector('.productPrice');
+const currentProductColors = document.querySelectorAll('.color');
+const currentProductSizes = document.querySelectorAll('.size');
+
+let chosenProduct = products[0];
+
+// event bubbling can be done here but it's only 5 elemets.
+menuItems.forEach((item, idx) => {
+  item.addEventListener('click', () => {
+    sliderWrapper.style.transform = `translateX(${-100 * idx}vw)`;
+
+    chosenProduct = products[idx];
+
+    currentProductTitle.textContent = chosenProduct.title;
+    currentProductPrice.textContent = `$${chosenProduct.price}`;
+    currentProductImg.src = chosenProduct.colors[0].img;
+
+    currentProductColors.forEach((color, idx) => {
+      color.style.backgroundColor = chosenProduct.colors[idx].code;
     });
   });
-};
+});
 
-const main = () => {
-  handleSlider();
-};
+currentProductColors.forEach((color, idx) => {
+  color.addEventListener('click', () => {
+    currentProductImg.src = chosenProduct.colors[idx].img;
+  });
+});
 
-main();
+let isChecked = false;
+currentProductSizes.forEach((size, idx) => {
+  size.addEventListener('click', () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = 'white';
+      size.style.color = 'black';
+    });
+
+    size.style.backgroundColor = 'black';
+    size.style.color = 'white';
+  });
+});
