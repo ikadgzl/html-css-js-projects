7201b1f6507e4f33bb13149163933b725acad180
ikadgzl <ikadgzl@gmail.com>
2022-02-04 23:45:51 +0300

    added navbar with responsive design

--- restaurant-website/src/App.js
+++ restaurant-website/src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+//question, img-div-4pxdif.
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
