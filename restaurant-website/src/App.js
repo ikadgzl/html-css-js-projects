07496331b011fa13d20533508cc875c5447b5833
ikadgzl <ikadgzl@gmail.com>
2022-02-08 02:36:19 +0300

    added aboutus

--- restaurant-website/src/App.js
+++ restaurant-website/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import AboutUs from './container/AboutUs/AboutUs';
 import Header from './container/Header/Header';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className='App'>
       <Navbar />
       <Header />
+      <AboutUs />
     </div>
   );
 }
--- restaurant-website/src/container/AboutUs/AboutUs.jsx
+++ restaurant-website/src/container/AboutUs/AboutUs.jsx
@@ -1,5 +1,48 @@
 import './AboutUs.css';
 
-const AboutUs = () => <div>AboutUs</div>;
+import { images } from '../../constants/';
+
+const AboutUs = () => (
+  <div
+    className='app__aboutus app__bg  flex__center section__padding'
+    id='about'
+  >
+    <div className='app__aboutus-overlay flex__center '>
+      <img src={images.G} alt='background G letter' />
+    </div>
+
+    <div className='app__aboutus-content flex__center'>
+      <div className='app__aboutus-content_about'>
+        <h1 className='headtext__cormorant'>About Us</h1>
+        <img className='spoon__img' src={images.spoon} alt='spoon' />
+        <p className='p__opensans'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni et
+          repellendus vitae sint saepe odio facilis doloribus accusamus laborum
+          assumenda!
+        </p>
+        <button type='button' className='custom__button'>
+          Know More
+        </button>
+      </div>
+
+      <div className='app__aboutus-content_knife flex__center'>
+        <img src={images.knife} alt='knife' />
+      </div>
+
+      <div className='app__aboutus-content_history'>
+        <h1 className='headtext__cormorant'>Our History</h1>
+        <img className='spoon__img' src={images.spoon} alt='spoon' />
+        <p className='p__opensans'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni et
+          repellendus vitae sint saepe odio facilis doloribus accusamus laborum
+          assumenda!
+        </p>
+        <button type='button' className='custom__button'>
+          Know More
+        </button>
+      </div>
+    </div>
+  </div>
+);
 
 export default AboutUs;
