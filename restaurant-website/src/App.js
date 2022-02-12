59ae6558ffd3c804c1bdae9a93e180fc8e37804e
ikadgzl <ikadgzl@gmail.com>
2022-02-12 18:07:03 +0300

    added special menu section

--- restaurant-website/src/App.js
+++ restaurant-website/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import AboutUs from './container/AboutUs/AboutUs';
 import Header from './container/Header/Header';
+import SpecialMenu from './container/Menu/SpecialMenu';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Header />
       <AboutUs />
+      <SpecialMenu />
     </div>
   );
 }
--- restaurant-website/src/components/Menuitem/MenuItem.jsx
+++ restaurant-website/src/components/Menuitem/MenuItem.jsx
@@ -1,5 +1,27 @@
 import './MenuItem.css';
 
-const MenuItem = () => <div>MenuItem</div>;
+const MenuItem = ({ title, price, tags }) => (
+  <div className='app__menuItem'>
+    <div className='app__menuItem-head'>
+      <div className='app__menuItem-name'>
+        <p className='p__cormorant' style={{ color: '#dcca87' }}>
+          {title}
+        </p>
+      </div>
+
+      <div className='app__menuItem-dash' />
+
+      <div className='app__menuItem-price'>
+        <p className='p__cormorant'>{price}</p>
+      </div>
+    </div>
+
+    <div className='app__menuItem-sub'>
+      <p className='p__opensans' style={{ color: '#aaa' }}>
+        {tags}
+      </p>
+    </div>
+  </div>
+);
 
 export default MenuItem;
